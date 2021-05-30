5e78aa2dc2ff0c41c5f193d7e2750ce37345b9c8
sagie-ro <sagie.rootst@gmail.com>
2021-05-30 19:14:33 +0300

    design

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -20,8 +20,22 @@ def assignment8():
     curr_user = {'first': "Sagie", 'last': "Rootshtain"}
     return render_template('assignment8.html',
                            curr_user=curr_user,
-                           Hobbies=['music','games','movies'],
+                           Hobbies=['music', 'games', 'movies'],
                            musicTypes=('rock', 'pop', 'indi'))
+
+
+@app.route('/assignment9', methods=['Get', 'Post'])
+def assignment9():
+    curr_user = {'first': "Sagie", 'last': "Rootshtain"}
+    if 'username' in request.args:
+        first_name = request.args['username']
+        last_name = request.args['lastname']
+    else:
+        first_name, last_name = '', ''
+    return render_template('assignment9.html',
+                           curr_user=curr_user,
+                           first_name=first_name,
+                           last_name=last_name)
 
 
 @app.route('/friends')
@@ -31,7 +45,6 @@ def friends():
                            curr_user=curr_user,
                            Hobbies=['music', 'games', 'movies'],
                            friends=['roni', 'dor', 'maya'])
-
 
 
 if __name__ == '__main__':
--- static/JS/cvfetch.js
+++ static/JS/cvfetch.js
@@ -3,18 +3,19 @@ function CreatUsersList(users){
     const user = users[0];
     console.log(user);
     const curr_main = document.querySelector("main");
-    for(let user of users){
-        const section = document.createElement('section');
-        section.innerHTML=`
-            <img src="${user.avatar}" alt="profile picture"/>
-        <div>
-            <span>${user.first_name} ${user.last_name}</span>
-            <br>
-                <a href="mailto:${user.email}">Email</a>
-        </div>`
-        curr_main.appendChild(section);
+        for(let user of users){
+            if (user.first_name= || user.last_name= || user.email=)
+                const section = document.createElement('section');
+                section.innerHTML=`
+                    <img src="${user.avatar}" alt="profile picture"/>
+                <div>
+                    <span>${user.first_name} ${user.last_name}</span>
+                    <br>
+                        <a href="mailto:${user.email}">Email</a>
+                </div>`
+                curr_main.appendChild(section);
 
-    }
+        }
 
 }
 fetch('https://reqres.in/api/users?page=2').then(
