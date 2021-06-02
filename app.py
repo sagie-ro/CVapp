b453162d422457a0e8c68b49282b6b10047a7636
sagie-ro <sagie.rootst@gmail.com>
2021-06-02 11:30:11 +0300

    assignment9

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
@@ -26,16 +26,34 @@ def assignment8():
 
 @app.route('/assignment9', methods=['Get', 'Post'])
 def assignment9():
-    curr_user = {'first': "Sagie", 'last': "Rootshtain"}
-    if 'username' in request.args:
-        first_name = request.args['username']
-        last_name = request.args['lastname']
+    curr_user = {'first': '', 'last': ''}
+    current_method = request.method
+    if 'username' in session:
+        username, lastname = session['username'], session['lastname']
     else:
-        first_name, last_name = '', ''
+        if current_method == 'GET':
+            if 'first_name' in request.args:
+                first_name = request.args['first_name']
+                last_name = request.args['last_name']
+            else:
+                first_name, last_name = '', ''
+        elif current_method == 'POST':
+            if 'first_name' in request.args:
+                first_name = request.form['first_name']
+                last_name = request.form['last_name']
+            else:
+                first_name, last_name = '', ''
+        else:
+            first_name, last_name = '', ''
+    if 'LogOut' in request.args:
+        username, lastname = '', ''
     return render_template('assignment9.html',
                            curr_user=curr_user,
+                           # username=username,
+                           # lastname=lastname,
                            first_name=first_name,
-                           last_name=last_name)
+                           last_name=last_name,
+                           current_method=current_method)
 
 
 @app.route('/friends')
--- static/JS/cvfetch.js
+++ static/JS/cvfetch.js
@@ -1,26 +1,30 @@
-function CreatUsersList(users){
+function CreatUsersList(users, first_name='', last_name='', email=''){
     console.log(users);
     const user = users[0];
     console.log(user);
     const curr_main = document.querySelector("main");
         for(let user of users){
-            if (user.first_name= || user.last_name= || user.email=)
-                const section = document.createElement('section');
+            let section;
+             // if (user.first_name === first_name || user.last_name === last_name || user.email === email || (first_name === '' & last_name === '' & email === ''))
+                section = document.createElement('section');
                 section.innerHTML=`
                     <img src="${user.avatar}" alt="profile picture"/>
                 <div>
                     <span>${user.first_name} ${user.last_name}</span>
                     <br>
-                        <a href="mailto:${user.email}">Email</a>
+                        <a href="mailto:${user.email}">${user.email}</a>
                 </div>`
                 curr_main.appendChild(section);
 
         }
 
 }
+  let first_name = document.getElementsByName("username");
+  let last_name = document.getElementsByName("lastname");
+  let email = document.getElementsByName("email");
 fetch('https://reqres.in/api/users?page=2').then(
     response=>response.json()
 ).then(
-    responseOBJECT=>CreatUsersList(responseOBJECT.data)
+    responseOBJECT=>CreatUsersList(responseOBJECT.data, first_name,last_name,email)
 )
     .catch(err => console.log(err));
